811567815650476140557b43e2f2cbd42c8c8e00
Teonta Pegues <153867380+codebyteonta@users.noreply.github.com>
2024-01-23 17:09:17 +0530

    Create Task.java

---
+++ Task.java
@@ -0,0 +1,27 @@
+// Task.java
+public class Task {
+    private String description;
+    private boolean completed;
+
+    public Task(String description) {
+        this.description = description;
+        this.completed = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void markAsCompleted() {
+        completed = true;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + (completed ? "X" : " ") + "] " + description;
+    }
+}
